7b126edf2e61e62a47db913a9088995cefdf1fe6
Jason Hang <jasonxhang@gmail.com>
2018-06-28 16:07:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(number){
   var newArray = []
   do {
-    
-  }
-  
+    newArray.push("I love the Beatles!")
+    number++
+  } while (number < 15)
+  return newArray;
 }
